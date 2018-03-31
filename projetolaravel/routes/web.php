109eaa3bf3099f7313b64d50e1054949d74f2115
User-PC\User <mauriciox8mm@hotmail.com>
2018-03-30 21:37:17 -0300

    aula 12 metade

--- projetolaravel/routes/web.php
+++ projetolaravel/routes/web.php
@@ -10,10 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+$this->get('/', 'SiteControle@index');
 
 Auth::routes();
 
