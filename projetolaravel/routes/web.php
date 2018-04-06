046635c80ff25437f1305652e04100fef9a41bdf
mauricio <mauriciox8mm@hotmail.com>
2018-04-06 15:44:12 -0300

    aula 12 8,32

--- projetolaravel/routes/web.php
+++ projetolaravel/routes/web.php
@@ -1,17 +1,8 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-$this->get('/', 'SiteControle@index');
 
+$this->get('admin', 'Admin\AdminController@index')->name('admin.home');
+//está puxando da pasta App/http/controlers sitecontroler comando para crialo php artisan make:controller nomedocontroler
+$this->get('/', 'SiteControle@index')->name('home');
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
