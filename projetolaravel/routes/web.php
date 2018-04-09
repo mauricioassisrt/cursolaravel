615b3ec60cd8759c1ebd00cf1cd011fbbdd2f48e
mauricio <mauriciox8mm@hotmail.com>
2018-04-09 15:07:34 -0300

    16 2 min

--- projetolaravel/routes/web.php
+++ projetolaravel/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+$this->group(['middleware' => ['auth'], 'namespace ' =>'Admin' ], function (){
+    $this->get('admin', 'Admin\AdminController@index')->name('admin.home');
 
-$this->get('admin', 'Admin\AdminController@index')->name('admin.home');
+});
+
 //está puxando da pasta App/http/controlers sitecontroler comando para crialo php artisan make:controller nomedocontroler
 $this->get('/', 'SiteControle@index')->name('home');
 Auth::routes();
